a6fa005feb4018c658db395d4dade14b4df243da
GDnos <gjbryan04@gmail.com>
2025-08-05 13:26:20 +0100

    main

--- src/main.c
+++ src/main.c
@@ -8,49 +8,140 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+#include "math.h"
+
+struct masslike {
+	Vector2 p;
+	Vector2 v;
+	Vector2 a;
+	Vector2 size;
+	double density;
+};
+
+#define n 2
+
+double getMass(struct masslike m) {
+	return m.density*m.size.x*m.size.y;
+}
+
+struct masslike masses[n];
+
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	InitWindow(1200,1200,"basic window");
+	SetTargetFPS(24);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	double time;
+	double timemax = 1000;
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	double dt = 0.03;
+	Vector2 positions[n][(int)(timemax/dt)];
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	// body setup
+	int i,j;
+	for(i = 0; i < n; i++)
+    {
+		masses[i].p = (Vector2){0,0};
+		masses[i].v = (Vector2){0,0};
+		masses[i].a = (Vector2){0,0};
+
+        masses[i].density = 1;
+        masses[i].size = (Vector2){0.02,0.02};
+    }
+
+	masses[0].density = 5;
+	masses[0].p = (Vector2){0.2, 0.2};
+	masses[0].v = (Vector2){0.0,0.0};
+
+	masses[1].density=5;
+	masses[1].p = (Vector2){0.5, 0.5};
+	masses[0].v = (Vector2){0.0,0.01};
+
+	for (time = 0; time < timemax; time += dt) {
+		for (i=0;i<n;i++) {
+			//Calculate mutual forces
+			#define m1 masses[i]
+			for (j=0;j<n;j++) {
+				if(i==j) {
+					continue;
+				}
+				
+				#define m2 masses[j]
+
+				m1.a = Vector2Scale(Vector2Normalize(Vector2Subtract(m2.p, m1.p)), (getMass(m2))/(Vector2Distance(m1.p,m2.p)));
+				m1.v = Vector2Add(m1.v, Vector2Scale(m1.a, dt));
+				
+				
+
+				//Check for collision
+
+				if (CheckCollisionRecs(
+					(Rectangle){m1.p.x, m1.p.y, m1.size.x, m1.size.y},
+					(Rectangle){m2.p.x, m2.p.y, m2.size.x, m2.size.y}
+				)) {
+					#define mass1 getMass(m1)
+					#define mass2 getMass(m2)
+
+					m1.v.x = m1.v.x*(mass1-mass2)/(mass1+mass2) + m2.v.x*(2*mass2)/(mass1+mass2);
+					m1.v.y = m1.v.y*(mass1-mass2)/(mass1+mass2) + m2.v.y*(2*mass2)/(mass1+mass2);
+					
+					m2.v.x = m1.v.x*(2*mass1)/(mass1+mass2) - m2.v.x*(mass1-mass2)/(mass1+mass2);
+					m2.v.y = m1.v.y*(2*mass1)/(mass1+mass2) - m2.v.y*(mass1-mass2)/(mass1+mass2);
+					
+
+					#undef mass1
+					#undef mass2
+				}
+
+				#undef m2
+				
+			}
+			if(m1.p.x>1-m1.size.x || m1.p.x<0) {
+				m1.v.x = -0.5*m1.v.x;
+				if(m1.p.x>1-m1.size.x) {
+					m1.p.x=1-m1.size.x;
+				}
+				if(m1.p.x<0) {
+					m1.p.x=0;
+				}
+			}
+			if(m1.p.y>1-m1.size.y || m1.p.y<0){
+				m1.v.y = -0.5*m1.v.y;
+				if(m1.p.y>1-m1.size.y) {
+					m1.p.y=1-m1.size.y;
+				}
+				if(m1.p.y<0) {
+					m1.p.y=0;
+				}
+			}
+
+			m1.p = Vector2Add(m1.p, Vector2Scale(m1.v, dt));
+			positions[i][(int)(time/dt)] = m1.p;
+
+			#undef m1
+		}
+	}
+	time = 0;
+	while (!WindowShouldClose())
 	{
-		// drawing
+		time = time + dt;
+
 		BeginDrawing();
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(WHITE);
-
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,BLACK);
+		ClearBackground(BLACK);
+		if (time < timemax) {
+			for(i=0;i<n;i++){
+				DrawRectangle(1200*positions[i][(int)(time/dt)].x,1200*positions[i][(int)(time/dt)].y,masses[i].size.x*1200,masses[i].size.y*1200,WHITE);
+			}
+			
+		}
 		
-		DrawGrid(10, 1);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		// DrawLine(100,100,120,120,BLUE);
 		EndDrawing();
 	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
