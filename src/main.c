a2850b1096f2eb8f9e3393ece0ddf4330d78dbc8
GDnos <gjbryan04@gmail.com>
2025-08-05 23:57:21 +0100

    finished emagsim

--- src/main.c
+++ src/main.c
@@ -14,138 +14,101 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "math.h"
 
-struct masslike {
+struct fieldlike {
 	Vector2 p;
-	Vector2 v;
-	Vector2 v0;
-	Vector2 a;
-	Vector2 size;
-	double density;
-	Color color;
+	Vector2 direction;
+	double Magnitude;
 };
 
-#define n 4
+struct chargelike {
+	Vector2 p;
+	double charge;
+};
 
-double getMass(struct masslike m) {
-	return m.density*m.size.x*m.size.y;
-}
+#define gridsize 50
+#define n 2
 
-struct masslike masses[n];
+struct fieldlike fieldlines[gridsize][gridsize];
+struct chargelike charges[n];
 
 int main ()
 {
 	InitWindow(1200,1200,"basic window");
 	SetTargetFPS(24);
 
-	double time;
-	double timemax = 1000;
-
-	double dt = 0.03;
-	Vector2 positions[n][(int)(timemax/dt)];
-
 	// body setup
-	int i,j;
-	for(i = 0; i < n; i++)
-    {
-		masses[i].p = (Vector2){((double)GetRandomValue(1,950))/1000,((double)GetRandomValue(1,950))/1000};
-		masses[i].v = (Vector2){0,0};
-		masses[i].a = (Vector2){0,0};
-		masses[i].v0 = (Vector2){0,0};
-		masses[i].color = WHITE;
+	int i,j,k;
 
-        masses[i].density = 1;
-        masses[i].size = (Vector2){0.02,0.02};
-    }
+	for(i=0;i<n;i++) {
+		charges[i].p = (Vector2){0.1,0.1};
+		charges[i].charge = 1;
+	}
 
-	masses[0].density = 1;
-	masses[0].p = (Vector2){0.2, 0.2};
-	masses[0].v = (Vector2){0.0,0.0};
-	masses[0].color = RED;
+	charges[0].p = (Vector2){0.5,0.4};
+	charges[1].p = (Vector2){0.5,0.6};
+	charges[1].charge = -1;
 
-	masses[1].density=1;
-	masses[1].p = (Vector2){0.5, 0.5};
-	masses[1].size = (Vector2){0.05,0.05};
-	masses[1].v = (Vector2){0.0,0.01};
-	masses[1].color = BLUE;
-
-	for (time = 0; time < timemax; time += dt) {
-		for (i=0;i<n;i++) {
-			//Calculate mutual forces
-			#define m1 masses[i]
-			for (j=0;j<n;j++) {
-				if(i==j) {
-					continue;
-				}
-				
-				#define m2 masses[j]
-
-				m1.a = Vector2Scale(Vector2Normalize(Vector2Subtract(m2.p, m1.p)), (getMass(m2))/(Vector2Distance(m1.p,m2.p)));
-				m1.v = Vector2Add(m1.v, Vector2Scale(m1.a, dt));
-				
-				
-
-				//Check for collision
-
-				if (CheckCollisionRecs(
-					(Rectangle){m1.p.x, m1.p.y, m1.size.x, m1.size.y},
-					(Rectangle){m2.p.x, m2.p.y, m2.size.x, m2.size.y}
-				)) {
-					#define mass1 getMass(m1)
-					#define mass2 getMass(m2)
-
-					m1.v.x = m1.v.x*(mass1-mass2)/(mass1+mass2) + m2.v0.x*(2*mass2)/(mass1+mass2);
-					m1.v.y = m1.v.y*(mass1-mass2)/(mass1+mass2) + m2.v0.y*(2*mass2)/(mass1+mass2);
-					
-
-					#undef mass1
-					#undef mass2
-				}
-
-				#undef m2
-				
-			}
-			if(m1.p.x>1-m1.size.x || m1.p.x<0) {
-				m1.v.x = -0.5*m1.v.x;
-				if(m1.p.x>1-m1.size.x) {
-					m1.p.x=1-m1.size.x;
-				}
-				if(m1.p.x<0) {
-					m1.p.x=0;
-				}
-			}
-			if(m1.p.y>1-m1.size.y || m1.p.y<0){
-				m1.v.y = -0.5*m1.v.y;
-				if(m1.p.y>1-m1.size.y) {
-					m1.p.y=1-m1.size.y;
-				}
-				if(m1.p.y<0) {
-					m1.p.y=0;
-				}
-			}
-
-			#undef m1
-		}
-		for (i=0;i<n;i++) {
-			#define m1 masses[i]
-			m1.p = Vector2Add(m1.p, Vector2Scale(m1.v, dt));
-			m1.v0 = m1.v;
-			positions[i][(int)(time/dt)] = m1.p;
+	for(i=0;i<gridsize;i++) {
+		for(j=0;j<gridsize;j++) {
+			#define fieldline fieldlines[i][j]
+			fieldline.p = (Vector2){(double)(i)/gridsize,(double)(j)/gridsize};
+			fieldline.direction = (Vector2){0,0};
+			fieldline.Magnitude = 0;
 		}
 	}
-	time = 0;
+
 	while (!WindowShouldClose())
 	{
-		time = time + 10*dt;
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-		if (time < timemax) {
-			for(i=0;i<n;i++){
-				DrawRectangle(1200*positions[i][(int)(time/dt)].x,1200*positions[i][(int)(time/dt)].y,masses[i].size.x*1200,masses[i].size.y*1200,masses[i].color);
+
+		for(i=0;i<gridsize;i++) {
+			for(j=0;j<gridsize;j++) {
+				#define fieldline fieldlines[i][j]
+				fieldline.direction = (Vector2){0,0};
+				Vector2 forceVector = (Vector2){0,0};
+				for(k=0;k<n;k++) {
+					#define chargebit charges[k]
+					if (Vector2Length(Vector2Subtract(fieldline.p, chargebit.p)) < 0.01) {
+						continue;
+					}
+					
+					forceVector = Vector2Add(forceVector, Vector2Scale(Vector2Normalize(Vector2Subtract(fieldline.p, chargebit.p)), chargebit.charge/(pow(Vector2Distance(chargebit.p,fieldline.p),2))));
+					#undef chargebit
+				}
+				fieldline.Magnitude = Vector2Length(forceVector);
+				fieldline.direction = Vector2Normalize(forceVector);
+				#undef fieldline
 			}
-			
 		}
-		
+
+		for(k=0;k<n;k++) {
+			DrawRectangle(1200*charges[k].p.x-20, 1200*charges[k].p.y-20, 40,40, RED);
+		}
+		double highestMag = 0;
+		for(i=0;i<gridsize;i++) {
+			for(j=0;j<gridsize;j++) {
+				#define fieldline fieldlines[i][j]
+				if (fieldline.Magnitude > highestMag) {
+					highestMag = fieldline.Magnitude;
+				}
+				#undef fieldline
+			}
+		}
+		for(i=0;i<gridsize;i++) {
+			for(j=0;j<gridsize;j++) {
+				#define fieldline fieldlines[i][j]
+				// double length = (fieldline.Magnitude);
+				double length = 0.01;
+				#define clr CLITERAL(Color) {255,255,255, (int)(255*pow((fieldline.Magnitude/(highestMag)), 0.5))}
+				// DrawRectangle(1200*fieldline.p.x, 1200*fieldline.p.y, 10,10, BLUE);
+				
+				DrawLine((int)(1200*fieldline.p.x), (int)(1200*fieldline.p.y), (int)(1200*fieldline.direction.x*0.01 + 1200*fieldline.p.x), (int)(1200*fieldline.direction.y*0.01+fieldline.p.y*1200), clr);
+				#undef clr
+				#undef fieldline
+			}
+		}
 		// DrawLine(100,100,120,120,BLUE);
 		EndDrawing();
 	}
